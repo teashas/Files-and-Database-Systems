3bb17961d7428dd5ad2b31d7dd38278d98a25eda
Saima Teasha <74995287+teashas@users.noreply.github.com>
2022-10-05 22:38:03 -0700

    Create soccer_down.sql

---
+++ soccer_down.sql
@@ -0,0 +1,10 @@
+-- Author: Saima Teasha
+-- Cource: CS317
+-- Assignment: Exercise 3B
+
+-- 2. Submit complete sql code in the form of a file with a .sql suffix that will remove all of your soccer league tables – e.g. soccer_down.sql
+
+DROP TABLES Players;
+DROP TABLES Games;
+DROP TABLES Teams;
+DROP TABLES Schedule;
