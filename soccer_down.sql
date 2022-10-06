0d7a3ccbb39e4b79a9f678427b9b25c4edc62ae7
Saima Teasha <74995287+teashas@users.noreply.github.com>
2022-10-05 23:11:56 -0700

    Update soccer_down.sql

--- soccer_down.sql
+++ soccer_down.sql
@@ -4,6 +4,7 @@
 
 -- 2. Submit complete sql code in the form of a file with a .sql suffix that will remove all of your soccer league tables – e.g. soccer_down.sql
 
+DROP TABLES Teams;
 DROP TABLES Players;
 DROP TABLES Games;
 DROP TABLES Schedule;
